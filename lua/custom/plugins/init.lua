f8561925de3043f2dae008033d554924eb52d16a
samjberg <sjberg14@gmail.com>
2026-05-16 00:12:03 -0700

    Installed jump plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,22 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    url = 'https://git.disroot.org/andyg/leap.nvim.git',
+    name = 'leap.nvim',
+    config = function()
+      local leap = require 'leap'
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '<S-J>', '<Plug>(leap)', { desc = 'Leap' })
+      vim.keymap.set('n', '<A-j>', '<Plug>(leap-from-window)', { desc = 'Leap from window' })
+
+      vim.keymap.set({ 'n', 'o' }, 'gs', '<Plug>(leap-remote)', { desc = 'Leap remote' })
+      vim.keymap.set({ 'n', 'o' }, 'gS', '<Plug>(leap-remote-linewise)', { desc = 'Leap remote linewise' })
+      vim.keymap.set({ 'x', 'o' }, 'ar', '<Plug>(leap-remote-text-object)', { desc = 'Leap around remote text object' })
+      vim.keymap.set({ 'x', 'o' }, 'ir', '<Plug>(leap-remote-inner-text-object)', { desc = 'Leap inner remote text object' })
+
+      leap.opts.preview = function(ch0, ch1, ch2) return not (ch1:match '%s' or (ch0:match '%a' and ch1:match '%a' and ch2:match '%a')) end
+    end,
+  },
+}
